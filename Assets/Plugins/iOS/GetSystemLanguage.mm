321abff2a81c2577a5548d93ad9d6e3cdc55d73d
volgmartin <volgmartin@gmail.com>
2018-09-11 23:54:47 +0300

    init

---
+++ Assets/Plugins/iOS/GetSystemLanguage.mm
@@ -0,0 +1,22 @@
+﻿/*
+ * This Plug in will return the system language code.
+ */
+extern "C"
+{
+	char* cStringCopy(const char* string)
+	{
+    	if (string == NULL)
+        	return NULL;
+    	char* res = (char*)malloc(strlen(string) + 1);
+    	strcpy(res, string);
+    	return res;
+	}
+
+	char* _getSystemLanguage()
+	{
+		NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+		NSArray *languages = [defaults objectForKey:@"AppleLanguages"];
+    	NSString *currentLanguage = [languages objectAtIndex:0];    
+    	return cStringCopy([currentLanguage UTF8String]);
+	}	
+}
---
+++ Assets/SystemLanguage.cs
@@ -0,0 +1,61 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+#elif UNITY_IOS
+using System.Runtime.InteropServices;
+#elif UNITY_STANDALONE_WIN
+using System.Runtime.InteropServices;
+using System.Globalization;
+#endif
+
+public sealed class SystemLanguage
+{
+    public SystemLanguage()
+    { }
+
+    public static string GetLocaleCode(string defaultLocaleCode = "en")
+    {
+        string localeCode = null;
+#if UNITY_ANDROID
+            try
+            {
+                using (var locale = new AndroidJavaClass("java.util.Locale"))
+                {
+                    using (var defaultLocale = locale.CallStatic<AndroidJavaObject>("getDefault"))
+                    {
+                        localeCode = defaultLocale.Call<string>("getLanguage");
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                localeCode = defaultLocaleCode;
+            }
+#elif UNITY_IOS
+            localeCode = _getSystemLanguage();
+            if (!string.IsNullOrEmpty(localeCode) && localeCode.Length > 2)
+            {
+                localeCode = localeCode.Substring(0, 2);
+            }
+#elif UNITY_STANDALONE_WIN        
+        localeCode = GetSystemCulture().TwoLetterISOLanguageName;
+#else
+        localeCode = defaultLocaleCode;
+#endif
+        return localeCode;
+    }
+
+#if UNITY_IOS
+        [DllImport("__Internal")]
+        private static extern string _getSystemLanguage();
+#endif
+
+#if UNITY_STANDALONE_WIN
+    [DllImport("KERNEL32.DLL")]
+    private static extern int GetSystemDefaultLCID();
+
+    private static CultureInfo GetSystemCulture()
+    {
+        return new CultureInfo(GetSystemDefaultLCID());
+    }
+#endif
+}
